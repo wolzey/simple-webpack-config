89ba895c71c30520073cbb483338a61f61858fe6
Ethan Wolz <ethan@creadiv.io>
2019-02-09 11:52:34 -0700

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  devtool: 'cheap-module-source-map',
+  entry: './src/index.js',
+  output: {
+    filename: 'static/js/bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  plugins: [
+    new BrowserSyncPlugin({
+      host: 'localhost',
+      port: 3000,
+      server: { baseDir: ['dist'] }
+    })
+  ]
+}
